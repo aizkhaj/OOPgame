1921fda29dff8faea20a300afdfaf657cb13e54e
Aizaz K <az_2099@hotmail.com>
2015-12-08 13:41:19 -0500

    Initial Commit

---
+++ OOPgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OOPgame
+//
+//  Created by Aizazuddin Khaja on 2015-12-08.
+//  Copyright © 2015 A2Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
